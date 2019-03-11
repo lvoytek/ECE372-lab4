b873bd4187905112fc475c7f19fed077c090930c
Lena Voytek <dvoytek@email.arizona.edu>
2019-03-11 11:06:14 -0700

    Initial

---
+++ lab4/src/main.cpp
@@ -0,0 +1,20 @@
+// Author:         
+// Net ID:         
+// Date:           
+// Assignment:     Lab 4
+//
+// Description: This file contains a programmatic overall description of the
+// program. It should never contain assignments to special function registers
+// for the exception key one-line code such as checking the state of the pin.
+//
+
+
+int main(){
+
+
+  while(1){
+
+  }
+
+  return 0;
+}
