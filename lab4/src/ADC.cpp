a3f3c970ff2c06237c5a3416dcdb7d572e99c5a4
Lena Voytek <dvoytek@email.arizona.edu>
2019-03-12 08:15:14 -0700

    Fixed Headers

--- lab4/src/ADC.cpp
+++ lab4/src/ADC.cpp
@@ -1,4 +1,4 @@
-// Authors: Nadine Alnajdawi, Jacob Bowles, Jessica Sofka, Lena Voytek    
+// Authors: Nadine Najdawi, Jacob Bowles, Jessica Sofka, Lena Voytek    
 // Net IDs: nadinealnajdawi, bowlesj, jsofka, dvoytek      
 // Date: 25 March 2019      
 // Assignment: Lab 4
