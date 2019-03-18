0691b55db31c2e69bec08e47bb7374c03eea6442
nalnajdawi <39809154+nalnajdawi@users.noreply.github.com>
2019-03-18 12:05:09 -0700

    Update switch.cpp

--- lab4/src/switch.cpp
+++ lab4/src/switch.cpp
@@ -9,19 +9,19 @@
  * Initializes pull-up resistor on PB3 and sets it into input mode
  */
 void initSwitch(){
-  // enable pin-change interrupts
+  DDRB &= ~(1 << DDB3);
+  PORTB |= (1 << PORTB3);
+
+  // PCICR |= (1 << PCIE0);      // enable PCINT for 7-0
+  //PCMSK0 |= (1 << PCINT5);    // enable PCINT for PCINT5
+}
+
+void initSwitchPB3(){
+  DDRB &= ~(1 << DDB3);
+  PORTB |= (1 << PORTB3); // enable pull-up for PORTB5
+
+  PCICR |= (1 << PCIE0);
   // enable interrupts on PB3
-  DDRB  &= ~(1 << DDB3);//initialize input for switch
-  PORTB |= (1 << PORTB3);// turn on switch, enable pull-up for PORTB3
-
-  PCICR  |= (1 << PCIE0);      // enable PCINT for 7-0
-  PCMSK0 |= (1 << PCINT3);    // enable PCINT for PCINT3
+  PCMSK0 |= (1 << PCINT3);
 }
 
-void switchOn() {
-
-}
-
-void switchOff() {
-
-}
