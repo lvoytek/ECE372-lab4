c0eee89947e9351790f20df24d42dc63b9035ff8
nalnajdawi <39809154+nalnajdawi@users.noreply.github.com>
2019-03-18 12:39:08 -0700

    Update main.cpp

--- lab4/src/main.cpp
+++ lab4/src/main.cpp
@@ -1,30 +1,67 @@
-// Authors: Nadine Najdawi, Jacob Bowles, Jessica Sofka, Lena Voytek    
-// Net IDs: nadinealnajdawi, bowlesj, jsofka, dvoytek      
-// Date: 25 March 2019      
-// Assignment: Lab 4
+// Author:         
+// Net ID:         
+// Date:          
+// Assignment:     Lab 4
 //
-// Description: This file contains a programmatic overall description of the
-// program. It should never contain assignments to special function registers
-// for the exception key one-line code such as checking the state of the pin.
-//
-#include "PWM.h"
+//----------------------------------------------------------------------//
+#include <Arduino.h>
+#include <avr/io.h>
+#include "adc.h"
+#include "switch.h"
 #include "timer.h"
+#include "pwm.h"
 
-int main()
-{
-    initPWM();
-    initTimer0();
+/*
+ * Define a set of states that can be used in the state machine using an enum.
+ */
 
-    int duty = 100;
-    while(1)
-    {
-      changeDutyCycle(duty);
-      delayMs(10);
-      duty--;
+typedef enum stateType_enum {
+  wait_press, debounce_press, wait_release, debounce_release
+  } stateType;
+volatile stateType state = wait_press;
+volatile bool button = true;
 
-      if(duty == 0)
-        duty = 100;
+int main(){
+  int low = 0;
+  sei();
+  initSwitchPB3();
+  initTimer0();
+  initADC();
+  initPWM();
+  while(1){
+    // Switch ISR
+    switch(state){
+      case wait_press:
+        break;
+      case debounce_press:
+        delayMs(1);
+        state = wait_release;
+        break;
+      case wait_release:
+        break;
+      case debounce_release:
+        delayMs(1);
+        state = wait_press;
+        break;
+      default:
+        break;
     }
-
+    if(button){
+      low = ADCL;
+      low += (unsigned int) (ADCH << 8);
+      changeDutyCycle(low);
+    }
+  }
   return 0;
+}
+
+//set PCINT
+ISR(PCINT0_vect){
+  if(state == wait_press){
+    state = debounce_press;
+  }
+  else if (state == wait_release){
+    button = ~button;
+    state = debounce_release;
+  }
 }
